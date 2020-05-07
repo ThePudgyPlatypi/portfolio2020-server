22cd048103a7ac2f03016a017c589bcd637460b9
Chris <stehmc1@tcnj.edu>
2020-05-07 01:22:41 -0400

    added post specific key and value

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 
 const app = express();
 
@@ -28,6 +28,17 @@ app.get('/api/pieces/:name', async (req, res) => {
         // pull up the correct collection and then find the object based off url param
         const pieceInfo = await db.collection('pieces').findOne({name: pieceName});
         // send status response and information 
+        res.status(200).json(pieceInfo);
+    }, res);
+});
+
+// GET - by ID
+app.get('/api/piece/:id', async (req, res) => {
+    withDB( async (db) => {
+        // need to use ObjectId() in order to convert to id that mongo can read? works.
+        const id = req.params.id;
+        const o_id = new ObjectId(id)
+        const pieceInfo = await db.collection('pieces').findOne({_id: o_id});
         res.status(200).json(pieceInfo);
     }, res);
 });
@@ -96,5 +107,41 @@ app.post('/api/pieces/:name/featured', async (req, res) => {
     }, res);
 });
 
+// update the whole object, expects the whole object
+app.post('/api/pieces/:title/update-piece', async (req, res) => {
+    withDB( async (db) => {
+        const pieceName = req.params.name;
+        const pieceInfo = await db.collection('pieces').findOne({name: pieceName});
+        await db.collection('pieces').updateOne({title: pieceName }, {
+            '$set' : {
+                name: req.body.name,
+                title: req.body.title,
+                images: req.body.images,
+                alt: req.body.alt,
+                shortDescription: req.body.shortDescription,
+                longDescription: req.body.longDescription,
+                features: req.body.features,
+                featured: req.body.featured
+            }
+        });
+        const updatedPieceInfo = await db.collection('pieces').findOne({name: pieceName});
+        res.status(200).json(updatedPieceInfo);
+    }, res);
+});
+
+// update just one value
+app.post('/api/pieces/:id/:key/update-piece', async (req, res) => {
+    withDB( async (db) => {
+        let dynamicSet = {};
+        const id = req.params.id;
+        const o_id = new ObjectId(id)
+        dynamicSet[req.params.key] = req.body.value;
+        await db.collection('pieces').updateOne({_id: o_id }, {
+            '$set' : dynamicSet
+        });
+        const updatedPieceInfo = await db.collection('pieces').findOne({_id: o_id});
+        res.status(200).json(updatedPieceInfo);
+    }, res);
+});
 
 app.listen(8000, () => console.log('Listening on port 8000'));
